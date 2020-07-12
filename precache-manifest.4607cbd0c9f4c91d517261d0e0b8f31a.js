3c349ce3015a96a51e29027daebd992ace940e84
Trevor <t.sontag@twosigma.com>
2020-07-12 16:39:20 -0400

    Add assets

---
+++ precache-manifest.4607cbd0c9f4c91d517261d0e0b8f31a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf852ec15ad854b52c21e08dbaa75f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581d28e6be129af25125",
+    "url": "/static/css/2.d81d1897.chunk.css"
+  },
+  {
+    "revision": "31632192ec1ed480358d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "581d28e6be129af25125",
+    "url": "/static/js/2.4ddda157.chunk.js"
+  },
+  {
+    "revision": "376b62a5554169952038aa9c410894b5",
+    "url": "/static/js/2.4ddda157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31632192ec1ed480358d",
+    "url": "/static/js/main.29ea09e1.chunk.js"
+  },
+  {
+    "revision": "75c7444118253df479f0",
+    "url": "/static/js/runtime-main.4243621b.js"
+  }
+]);
